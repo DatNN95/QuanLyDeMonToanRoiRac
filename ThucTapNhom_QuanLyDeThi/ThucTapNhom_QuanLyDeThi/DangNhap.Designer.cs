06232db4c9c0c118cee8bac44304264c6b344c07
GitHub123Dat <ngocdatnguyen1995@gmail.com>
2018-05-17 07:07:50 +0700

    code C#

---
+++ ThucTapNhom_QuanLyDeThi/ThucTapNhom_QuanLyDeThi/DangNhap.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ThucTapNhom_QuanLyDeThi
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.txtuserr = new System.Windows.Forms.Label();
+            this.txtPassWord = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(157, 175);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 0;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(262, 175);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(75, 23);
+            this.btnDangKy.TabIndex = 1;
+            this.btnDangKy.Text = "Thoát";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // txtuserr
+            // 
+            this.txtuserr.AutoSize = true;
+            this.txtuserr.Location = new System.Drawing.Point(57, 56);
+            this.txtuserr.Name = "txtuserr";
+            this.txtuserr.Size = new System.Drawing.Size(57, 13);
+            this.txtuserr.TabIndex = 2;
+            this.txtuserr.Text = "UserName";
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.AutoSize = true;
+            this.txtPassWord.Location = new System.Drawing.Point(56, 101);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(56, 13);
+            this.txtPassWord.TabIndex = 3;
+            this.txtPassWord.Text = "PassWord";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(157, 56);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(180, 20);
+            this.txtUser.TabIndex = 4;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(157, 101);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(180, 20);
+            this.txtPass.TabIndex = 5;
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 296);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPassWord);
+            this.Controls.Add(this.txtuserr);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.btnDangNhap);
+            this.Name = "DangNhap";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Label txtuserr;
+        private System.Windows.Forms.Label txtPassWord;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
+
